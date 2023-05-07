4f3825c137eeb200c043d4dd892124d3f1274b0c
Carlos Del Castillo M <maussac13@gmail.com>
2023-05-07 15:15:58 -0500

    init proyecto veterinaria

---
+++ src/main/java/com/softcaribbean/veterinaria/VeterinariaBackEndFinalApplication.java
@@ -0,0 +1,13 @@
+package com.softcaribbean.veterinaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeterinariaBackEndFinalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VeterinariaBackEndFinalApplication.class, args);
+    }
+
+}
